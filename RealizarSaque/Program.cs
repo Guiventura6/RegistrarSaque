624ac7d621f872b2d4a7d3f0e617f3f2da044d49
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-01 16:54:29 -0300

    Implementacao do saque no arquivo program, sem validacoes

--- RealizarSaque/Program.cs
+++ RealizarSaque/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using RealizarSaque.Entities;
 
 namespace RealizarSaque
 {
@@ -6,7 +8,27 @@ namespace RealizarSaque
     {
         static void Main(string[] args)
         {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            //Entrada
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Withdraw limit: ");
+            double withDrawLimit = double.Parse(Console.ReadLine(), CI);            
+            Account acc = new Account(number, holder, initialBalance, withDrawLimit);
             
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double withDraw = double.Parse(Console.ReadLine(), CI);
+            acc.WithDraw(withDraw);
+            //Saida
+            Console.WriteLine($"New balance: {acc.Balance.ToString("F2", CI)}"); 
+
+
         }
     }
 }
